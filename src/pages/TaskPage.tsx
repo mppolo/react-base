ab35def0304d3be4e3f8d25be215a422bd76fc92
Marco Polo <mppolo@icloud.com>
2025-06-12 20:55:12 -0300

    commit test

--- src/pages/TaskPage.tsx
+++ src/pages/TaskPage.tsx
@@ -38,6 +38,7 @@ function TaskPage() {
        const response = await fetch('https://jsonplaceholder.typicode.com/todos?_limit=10', {
         method: 'GET'
       })
+      //
 
       const data = await response.json()
 
